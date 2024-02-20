58d28b56eb6823c5efa06b407338c6b18748d19c
Felipe Zuntini Abella <felzun@br.ibm.com>
2024-02-20 17:03:13 -0300

    feat: 🎸 change logo from storybook

---
+++ .storybook/ZuntiniTheme.ts
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+	base: 'dark',
+	brandTitle: 'Zuntini dot Ninja',
+	brandUrl: 'https://zuntini.ninja',
+	brandImage: 'https://tweenn.github.io/static/logo.svg',
+	brandTarget: '_blank',
+});
---
+++ .storybook/manager.ts
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/manager-api';
+import zuntiniTheme from './ZuntiniTheme';
+
+addons.setConfig({
+	theme: zuntiniTheme,
+});
